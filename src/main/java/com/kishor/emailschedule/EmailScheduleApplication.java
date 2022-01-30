5d5b5d3d3df14da75634b802abf80d8bb613157f
Kishor05 <kishor.b@joshsoftware.com>
2022-01-30 18:52:08 +0530

    scheduling mail

---
+++ src/main/java/com/kishor/emailschedule/EmailScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.kishor.emailschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailScheduleApplication.class, args);
+	}
+
+}
